e581bc62085d04cbd85e6eefd57130c8b9fcc3b1
satyarth.ojha <satyarth.ojha@grabpay.com>
2020-07-15 20:17:34 +0530

    changed url

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use('/',router)
 
 const userSchema = require('./models/userModel')
 
-router.post('/',(req,res)=>{
+router.post('/add',(req,res)=>{
     const name = req.body.name
     const newUser = new userSchema({
         _id: new mongoose.Types.ObjectId(),
@@ -41,7 +41,7 @@ router.post('/',(req,res)=>{
 
 })
 
-router.get('/',(req,res)=>{
+router.get('/fetch',(req,res)=>{
     userSchema.find()
     .exec()
     .then(docs=>{
